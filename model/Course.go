8c0a6dba4a0c173b8e1c36bd769e44d95770d0f4
mpzleon <42512444+mpzleon@users.noreply.github.com>
2020-06-17 20:34:58 +0800

    Create Course.go

---
+++ model/Course.go
@@ -0,0 +1,13 @@
+package main
+ 
+import  "fmt"
+
+ 
+ 
+type Admin struct {  
+    Id int `json:"Id"`
+	Name string `json:"Name"`
+	Time string`json:"Time"`
+	PeopleNumber string `json:"PeopleNumber"`
+}
+ 
