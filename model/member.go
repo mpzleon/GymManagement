48527246a0abb3f2523bb03e633b3f91d313ab15
mpzleon <42512444+mpzleon@users.noreply.github.com>
2020-06-17 20:28:18 +0800

    Update member.go

---
+++ model/member.go
@@ -0,0 +1,17 @@
+package main
+ 
+import  "fmt"
+
+ 
+ 
+type Member struct {  
+    Id int `json:"Id"`
+	Name string `json:"Name"`
+	Gender string`json:"Gender"`
+	Password string`json:"Password"`
+	Age int`json:"Age"`
+	PhoneNumber int`json:"PhoneNumber"`
+	Address string`json:"Address"`
+	term  int `json:"term"`
+}
+ 
